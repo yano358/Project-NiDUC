9762ac4d5a5b2bb0336b8ead3aa99ddf85b3ccf9
Huntarman <105349936+Huntarman@users.noreply.github.com>
2023-04-24 14:58:11 +0200

    "create a csv file for tests + some fixes" (#4)

--- AwardPoints.py
+++ AwardPoints.py
@@ -2,6 +2,7 @@ from math import atan2, sqrt, pi
 
 
 pts = {
+    0:13,
     1:13,
     2:4,
     3:18,
@@ -28,34 +29,41 @@ pts = {
 def calculatePoints(xCord, yCord):
     angle = calcAngle(xCord,yCord)
     #calculates which area would the shot land in
+    area = 0
     for i in range(9,360,18):
         if angle > (i % 360) and angle <= ((i + 18)%360):
             area = 1 + (i - 9) / 18
     #checks for amount of points in area shot landed in
-    points = pts(area)
-        
+    points = pts[area]
+    
     #calculates the modifier of points, 0 if the shot was beyond the radius
     line_length = sqrt(xCord*xCord + yCord * yCord)
     if line_length <= 0.5:
         points = 50
         point_mod = 1
+        multInfo = False
 
     elif line_length <= 3.0 :
         point_mod = 1
+        multInfo = False
 
     elif line_length <= 3.25:
         point_mod = 3
+        multInfo = True
 
     elif line_length <= 4.75:
         point_mod = 1
+        multInfo = False
 
     elif line_length <= 5.0:
         point_mod = 2
-    
+        multInfo = True
+
     else:
         point_mod = 0
+        multInfo = False
 
-    return points * point_mod
+    return points * point_mod, multInfo
 
 #returns angle of a line between the point and (0,0)
 def calcAngle(xCord, yCord):
--- CreatePoints.py
+++ CreatePoints.py
@@ -1,51 +1,64 @@
 from math import sin, cos, pi, log1p
 from matplotlib import pyplot as plt
-
 #this is only used for generation of gaussian distribution
 import random as rd
 
 #our random value generator
 from rng import random_value
 import AwardPoints
-
+from createCsv import create
+#Legacy comments
 #test params below, sg ideal ranges-> >1 to 4:
 #sg is an inverse of sigma so we can have the logic of bigger number -> more accurate machine
-sg = 0.2
-
 #This will create a distribution of points on a circle based on the "sg"
+
 def distribute():
     points = []
     coords = generateCircleArray() #create circle
-    #one round has 3 throws * 8 rounds in a match
-    for y in range(24):
-        randCoord = random_value(len(coords)-1) #pick random index of coords
-        gaussSample = generateGauss()
+    shotsPerRound = 24 
+    sigCurr = 1.0
+    sigBegin = sigCurr
+    sigEnd = 2.5
+    #one round has (shotsPerRound) throws * ((sigEnd - sigCurr) / 0.1) rounds in a match
+    while round(sigCurr,1) <= sigEnd:
+        for y in range(shotsPerRound):
+            randCoord = random_value(len(coords)-1) #pick random index of coords
+            gaussSample = generateGauss(sigCurr)
 
-        value = coords[randCoord] #value to multiply
+            value = coords[randCoord] #value to multiply
 
-        #move the point toward the center
-        xCord = value[0]*gaussSample
-        yCord = value[1]*gaussSample
+            #move the point toward the center
+            xCord = value[0]*gaussSample
+            yCord = value[1]*gaussSample
 
-        #compression for mor randomness
-        if(random_value(10) >= 8):
-            yCord = cordCompression(yCord)
-            plt.plot(xCord,yCord,marker="o",markersize=5,markeredgecolor="black",markerfacecolor="black")
-            continue
-        if(random_value(10)>= 8):
-            xCord = cordCompression(yCord)
-            plt.plot(xCord,yCord,marker="o",markersize=5,markeredgecolor="yellow",markerfacecolor="yellow")
-            continue
+            #compression for mor randomness
+            if(random_value(10) >= 8):
+                yCord = cordCompression(yCord)
+                plt.plot(xCord,yCord,marker="o",markersize=5,markeredgecolor="black",markerfacecolor="black")
+                points.append(AwardPoints.calculatePoints(xCord=xCord,yCord=yCord))
+                continue
+            if(random_value(10)>= 8):
+                xCord = cordCompression(yCord)
+                plt.plot(xCord,yCord,marker="o",markersize=5,markeredgecolor="yellow",markerfacecolor="yellow")
+                points.append(AwardPoints.calculatePoints(xCord=xCord,yCord=yCord))
+                continue
         
-        #add to graph
-        plt.plot(xCord, yCord, marker="o", markersize=5, markeredgecolor="blue", markerfacecolor="green")
+            #add to graph
+            plt.plot(xCord, yCord, marker="o", markersize=5, markeredgecolor="blue", markerfacecolor="green")
 
-        #points.append(AwardPoints.calculatePoints(xCord=xCord,yCord=yCord))
+            points.append(AwardPoints.calculatePoints(xCord=xCord,yCord=yCord))
+        
+        sigCurr += 0.1
+        
 
+    create(sigBegin = sigBegin, sigEnd = sigEnd, shotsPerRound = shotsPerRound , points = points)
+    
     #plot circle
     for x in range(len(coords)):
         plt.plot((coords[x][0]), coords[x][1], marker="o", markersize=5, markeredgecolor="red", markerfacecolor="green")
     plt.show()
+    print(len(points))
+    print(points)
 
 #generates coordinates of a circle
 def generateCircleArray():
@@ -60,7 +73,7 @@ def generateCircleArray():
     return coords
 
 #generate gaussian distribution with mean 0 and stddev of sg^-1
-def generateGauss():
+def generateGauss(sg):
     sample = rd.gauss(0.0,1/sg)
     while sample > 1 or sample < -1:
         sample = rd.gauss(0.0,sg)
